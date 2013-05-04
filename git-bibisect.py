365bf4373b012108a1709c47692b82417930a7e3
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2013-05-05 01:02:34 +0200

    Switched to python 3 and backup head

--- git-bibisect.py
+++ git-bibisect.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 #  git-bibisect is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -22,7 +22,6 @@ import os
 import sys
 
 def call_in(cmd, directory):
-    print "Calling %s in %s" % (cmd, directory)
     d = os.getcwd()
     os.chdir(directory)
     ret = subprocess.call(cmd.split(" "))
@@ -37,7 +36,7 @@ class BuildJob:
         self._cmds = cmds
 
     def checkout(self):
-        cmd = "git checkout %s" % self._rev
+        cmd = "git checkout -q %s" % self._rev
         # call in cwd
         subprocess.call(cmd.split(" "))
 
@@ -66,50 +65,82 @@ class BuildJob:
             cmd = "git add %s" % name
             ret = call_in(cmd, self._dest)
             if ret > 0:
-                print "Error: Could not add %s" % dest
+                print("Error: Could not add %s" % dest)
 
-        cmd = "git commit -m build_%s" % self._rev
+        cmd = "git commit -q -m build_%s" % self._rev
         ret = call_in(cmd, self._dest)
         if ret > 0:
-            print "Error: Could not commit %s" % self._rev
+            print("Error: Could not commit %s" % self._rev)
+
+def get_head():
+    head = ""
+    try:
+        cmd = "git symbolic-ref HEAD"
+        tmp = subprocess.check_output(cmd.split(" ")).decode(sys.stdout.encoding).strip()
+        head = tmp.replace("refs/heads/", "")
+    except subprocess.CalledProcessError:
+        cmd = "git show-ref HEAD"
+        tmp = subprocess.check_output(cmd.split(" ")).decode(sys.stdout.encoding).strip()
+        head = tmp.split(" ")[0]
+    return head
+
+def is_dirty():
+    cmd = "git status --porcelain"
+    tmp = subprocess.check_output(cmd.split(" ")).decode(sys.stdout.encoding).strip()
+    print(tmp)
+    return (tmp != "")
 
 def build(revs, files, cmds, dest):
+    # backup head
+    head = get_head()
+
     os.makedirs(dest)
-    call_in("git init .", dest)
+    call_in("git init -q .", dest)
 
     for r in revs:
         j = BuildJob(dest, r, files, cmds)
         j.checkout()
         if j.configure() and j.build():
             if not j.run():
-                print "Error: Could not run!"
+                print("Error: Could not run!")
             j.commit()
 
+    # restore head
+    cmd = "git checkout -q %s" % head
+    subprocess.call(cmd.split(" "))
+
+
 if __name__ == '__main__':
+    if is_dirty():
+        print("Warning: You should not run this on a dirty working tree.")
+
     # git bibisect --range HEAD^^..HEAD file1 file2 ... dest-dir
     n = len(sys.argv) - 1
     args = sys.argv[1:]
     s = 0
 
     if n == 0:
-        print "Error: No destination given."
+        print("Error: No destination given.")
+        sys.exit(1)
 
     cmd = "git rev-list HEAD"
     if n >= 2 and args[0] == "--range":
         cmd = "git rev-list --reverse %s" % args[1]
         # first 2 entries parsed
         s += 2
-    revs = subprocess.check_output(cmd.split(" ")).split("\n")
-    revs = [r for r in revs if r != '']
+    revs = subprocess.check_output(cmd.split(" ")).split(b"\n")
+    revs = [r.decode(sys.stdout.encoding) for r in revs if r != '']
 
     if s >= n:
-        print "Error: No destination given."
+        print("Error: No destination given.")
+        sys.exit(1)
 
     files = args[s:n-1]
     s += len(files)
 
     if s >= n:
-        print "Error: No destination given."
+        print("Error: No destination given.")
+        sys.exit(1)
 
     dest = args[n-1]
 
